fbbf7a6d6724cd085f5e3e636e4d13f047dec4b8
romzyyy <fijay0117@gmail.com>
2023-09-21 07:17:23 +0700

    style: refactore code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontWeight: {
+			thin: '100',
+			extralight: '200',
+			light: '300',
+			normal: '400',
+			medium: '500',
+			semibold: '600',
+			bold: '700',
+			extrabold: '800',
+			black: '1000',
+		  },
 		extend: {},
 	},
 	plugins: [],
