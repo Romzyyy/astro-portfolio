2141453c3f6c8d1c5753634372d3ddbf743b4e26
romzyyy <fijay0117@gmail.com>
2023-09-28 22:08:33 +0700

    style: add bg header

--- src/js/SkillAndTools.js
+++ src/js/SkillAndTools.js
@@ -4,6 +4,12 @@ const tabContent = document.querySelectorAll('[data-content]')
 tabs.forEach((tab) => {
     tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.target)
+
+        tabs.forEach((t) => {
+            t.classList.remove('active');
+        });
+        tab.classList.add('active');
+
         tabContent.forEach((tc) => {
             tc.classList.add('hidden')
         })
