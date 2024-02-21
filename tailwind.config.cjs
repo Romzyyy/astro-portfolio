30619b6e8b9c85f6e94935f4540d2af485f864f7
romzyyy <fijay0117@gmail.com>
2024-02-21 22:44:00 +0700

    style: add hover icon

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,26 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		fontWeight: {
-			thin: '100',
-			extralight: '200',
-			light: '300',
-			normal: '400',
-			medium: '500',
-			semibold: '600',
-			bold: '700',
-			extrabold: '800',
-			black: '1000',
-		  },
-		extend: {
-			fontFamily: {
-				sans: ["InterVariable", "Inter",],
-			  },
-		},
-	},
-	plugins: [
-		require('tailwind-scrollbar-hide')
-	],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        fontWeight: {
+            thin: '100',
+            extralight: '200',
+            light: '300',
+            normal: '400',
+            medium: '500',
+            semibold: '600',
+            bold: '700',
+            extrabold: '800',
+            black: '1000',
+        },
+        extend: {
+            fontFamily: {
+                sans: ['InterVariable', 'Inter'],
+            },
+            fill: ['hover', 'focus'],
+        },
+    },
+    plugins: [require('tailwind-scrollbar-hide')],
 }
